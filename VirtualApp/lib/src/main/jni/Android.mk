0dd51843d2a79bc19842fcca1a7fb45348b0dc92
v_xwuzhang <xinwuzhang@21kunpeng.com>
2021-03-03 10:39:51 +0800

    build successful

--- VirtualApp/lib/src/main/jni/Android.mk
+++ VirtualApp/lib/src/main/jni/Android.mk
@@ -134,6 +134,6 @@ LOCAL_SRC_FILES := Jni/VAJni.cpp \
                    utils/zMd5.cpp \
                    utils/controllerManagerNative.cpp
 endif
-LOCAL_LDLIBS := -llog -latomic
+LOCAL_LDLIBS += -llog -latomic
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
